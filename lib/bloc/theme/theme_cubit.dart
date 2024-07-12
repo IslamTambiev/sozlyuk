4eebf3900976e49902f74654ebe8d6129db6c8d2
IslamT1 <90341747+IslamT1@users.noreply.github.com>
2024-07-12 21:28:27 +0300

    saving theme settings added

--- lib/bloc/theme/theme_cubit.dart
+++ lib/bloc/theme/theme_cubit.dart
@@ -1,13 +1,31 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sozlyuk/repositories/settings/settings.dart';
 
 part 'theme_state.dart';
 
 class ThemeCubit extends Cubit<ThemeState> {
-  ThemeCubit() : super(const ThemeState(Brightness.light));
+  ThemeCubit({
+    required SettingsRepositoryInterface settingsRepository,
+  })  : _settingsRepository = settingsRepository,
+        super(const ThemeState(Brightness.light)) {
+    _checkSelectedTheme();
+  }
 
-  void setThemeBrightness(Brightness brightness) {
+  final SettingsRepositoryInterface _settingsRepository;
+
+  Future<void> setThemeBrightness(Brightness brightness) async {
     emit(ThemeState(brightness));
+    await _settingsRepository.setDarkThemeSelected(
+      brightness == Brightness.dark,
+    );
+  }
+
+  void _checkSelectedTheme() async {
+    final selected = await _settingsRepository.isDarkThemeSelected()
+        ? Brightness.dark
+        : Brightness.light;
+    emit(ThemeState(selected));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sozlyuk/bloc/theme/theme_cubit.dart';
+import 'package:sozlyuk/repositories/settings/settings.dart';
 import 'package:sozlyuk/router/router.dart';
 import 'package:sozlyuk/ui/theme/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
   //await DB.init();
-  runApp(const MyApp());
+  runApp(MyApp(preferences: prefs));
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.preferences});
+
+  final SharedPreferences preferences;
+
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -28,10 +34,11 @@ class _MyAppState extends State<MyApp> {
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Colors.black38, // Change the color here
     ));
+    final settingsRepository = SettingsRepository(preferences: widget.preferences);
 
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context) => ThemeCubit()),
+        BlocProvider(create: (context) => ThemeCubit(settingsRepository: settingsRepository)),
       ],
       child: BlocBuilder<ThemeCubit, ThemeState>(
         builder: (context, state) {
