58e032e74006a634af0abe0765847039ee521e37
IslamT1 <90341747+IslamT1@users.noreply.github.com>
2024-07-22 13:29:08 +0300

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -10,8 +11,14 @@ import 'package:sozlyuk/repositories/settings/settings.dart';
 import 'package:sozlyuk/router/router.dart';
 import 'package:sozlyuk/ui/theme/theme.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final SharedPreferences prefs = await SharedPreferences.getInstance();
   //await DB.init();
